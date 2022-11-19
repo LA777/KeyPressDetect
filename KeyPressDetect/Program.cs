2dcf1491005f354a7fdc874ee6ef5667a7ad2691
Alex L <LA.xdna@gmail.com>
2022-11-19 16:41:06 +0200

    initial commit

---
+++ KeyPressDetect/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+while (true)
+{
+    var key = Console.ReadKey();
+
+    Console.WriteLine(key.Key);
+    Console.WriteLine(key.KeyChar);
+    Console.WriteLine(key.Modifiers);
+}
+
+
